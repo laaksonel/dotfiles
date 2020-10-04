c4207cb2c25d6b11610fa33006218b1d588fa636
Lauri Laaksonen <lauri.k.laaksonen@gmail.com>
2020-10-04 20:00:19 +0300

    All fonts, added autoconfirms, polybar+rofi copies

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 sudo apt update
 
 # Misc
-sudo apt install git neovim dconf-editor tmux curl cmake
+sudo apt install -y git neovim dconf-editor tmux curl cmake
 # Plugins need to be installed manually (Vimium)
-sudo apt install chromium-browser
+sudo apt install -y chromium-browser
 setxkbmap -option caps:swapescape
 
 # i3
-sudo apt install i3
+sudo apt install -y i3
 
 # Relogin in order to generate default i3 config folder
 
@@ -20,9 +20,9 @@ cp ~/.bashrc ~/.profile ~/backup
 #sudo apt install build-essential clang-6.0 cmake pkg-config python3 python3-sphinx
 #sudo apt install libcairo2-dev libiw-dev libpulse-dev libxcb-composite0-dev libxcb-randr0-dev libxcb1-dev
 #libxcb-util0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
-sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+sudo apt install -y build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
 
-sudo apt install libcairo2-dev libiw-dev libpulse-dev i3-wm libjsoncpp-dev
+sudo apt install -y libcairo2-dev libiw-dev libpulse-dev i3-wm libjsoncpp-dev
 mkdir -p ~/tools/polybar
 git clone https://github.com/polybar/polybar.git ~/tools/polybar
 cd ~/tools/polybar
@@ -48,8 +48,8 @@ wget -P ~/.config/nvim https://raw.githubusercontent.com/laaksonenl/dotfiles/mas
 wget -P ~ https://raw.githubusercontent.com/laaksonenl/dotfiles/master/.vimrc
 
 # Coc requires Node
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install -y nodejs
+sudo apt install -y npm
 
 # Vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -61,59 +61,58 @@ wget -P ~ https://raw.githubusercontent.com/laaksonenl/dotfiles/master/.tmux.con
 tmux source ~/.tmux.conf
 
 # Autojump
-sudo apt install autojump
+sudo apt install -y autojump
 
 # Fuzzy finder
-sudo apt install fzf
+sudo apt install -y fzf
 
 # Ripgrep
-sudo apt install ripgrep
+sudo apt install -y ripgrep
 
 # i3 wallpaper
-sudo apt install feh
-cp ~/.config/polybar/wallpapers/bg-secondary.jpg ~/Pictures
+sudo apt install -y feh
 
 # Monitor settings
-sudo apt install arandr
+sudo apt install -y arandr
 
 # Appearance settings
-sudo apt install lxappearance
+sudo apt install -y lxappearance
 
 # File browser
-sudo apt install thunar
-sudo apt install ranger
+sudo apt install -y thunar
+sudo apt install -y ranger
 
 # Fix icons in thunar
-sudo apt install gnome-icon-theme
+sudo apt install -y gnome-icon-theme
 
 # Theme and icons
-sudo apt install arc-theme
-sudo add-apt-repository -u ppa:snwh/ppa
-sudo apt install moka-icon-theme faba-icon-theme
+sudo apt install -y arc-theme
+sudo add-apt-repository -y -u ppa:snwh/ppa
+sudo apt install -y moka-icon-theme faba-icon-theme
 
 # Command execution
-sudo apt install rofi
+sudo apt install -y rofi
 
 # Opaque backgrounds
-sudo apt install compton
+sudo apt install -y compton
 
 # Font for vim
-sudo apt install fonts-firacode
+sudo apt install -y fonts-firacode
 
 # Fast screenshots
 sudo add-apt-repository -y ppa:linuxuprising/shutter
-sudo apt install shutter
+sudo apt install -y shutter
 
 # Battery life for i3 bar
-sudo apt install acpi
+sudo apt install -y acpi
 
 # Icons for i3 bar
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt-get update
-sudo apt-get install papirus-icon-theme
+sudo add-apt-repository -y ppa:papirus/papirus
+sudo apt update
+sudo apt install -y papirus-icon-theme
 
 mkdir -p ~/projects
-git clone https://github.com/laaksonenl/dotfiles ~/projects
+git clone https://github.com/laaksonenl/dotfiles ~/projects/dotfiles
 
 cp ~/projects/dotfiles/.gtkrc-2.0 ~
 
@@ -123,3 +122,10 @@ cp ~/projects/dotfiles/.profile ~
 cp ~/projects/dotfiles/.bashrc ~
 
 cp -r ~/projects/dotfiles/fonts ~/.fonts
+fc-cache -v
+
+cp -r ~/projects/dotfiles/polybar ~/.config
+mv ~/.config/launch-polybar.sh ~/.config/lauch.sh
+
+mkdir -p ~/.config/rofi
+cp ~/projects/dotfiles/rofi/config ~/.config/rofi
